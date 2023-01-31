05d5fb03c58b9998ea788234e07b4f410f1deceb
DenVas123 <denis.vasilyev603@gmail.com>
2023-01-30 21:10:52 -0800

    added auto yes to setup.sh

--- setup.sh
+++ setup.sh
@@ -15,14 +15,14 @@ wget -P ./linux/Arm https://people.debian.org/~aurel32/qemu/armhf/initrd.img-3.2
 wget -P ./linux/Arm https://people.debian.org/~aurel32/qemu/armhf/vmlinuz-3.2.0-4-vexpress
 
 
-sudo apt install binwalk
-sudo apt install qemu-system-mips
-sudo apt install qemu-system-arm
-sudo apt install python 
-sudo apt install python3
-sudo apt install python-pip
-sudo pip install nose coverage
-sudo apt-get install libqt4-opengl python3-opengl python3-pyqt4 python3-pyqt4.qtopengl python3-numpy python3-scipy python3-pip
-sudo apt-get install mtd-utils gzip bzip2 tar arj lhasa p7zip p7zip-full cabextract cramfsswap squashfs-tools sleuthkit default-jdk lzop srecord
-sudo apt install sshpass
+sudo apt install binwalk -y
+sudo apt install qemu-system-mips -y
+sudo apt install qemu-system-arm -y
+sudo apt install python -y
+sudo apt install python3 -y
+sudo apt install python-pip -y
+sudo pip install nose coverage -y
+sudo apt-get install libqt4-opengl python3-opengl python3-pyqt4 python3-pyqt4.qtopengl python3-numpy python3-scipy python3-pip -y
+sudo apt-get install mtd-utils gzip bzip2 tar arj lhasa p7zip p7zip-full cabextract cramfsswap squashfs-tools sleuthkit default-jdk lzop srecord -y
+sudo apt install sshpass -y
 
