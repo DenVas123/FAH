ad41f41ca55c3d060f054beea79102a68423df4a
DenVas123 <denis.vasilyev603@gmail.com>
2023-01-30 20:19:49 -0800

    fixed run.sh

--- run.sh
+++ run.sh
@@ -15,12 +15,12 @@ fi
 com
 
 if [ "${1}" = "mips" ]; then
-	sudo qemu-system-mips -M malta -m 1024 -kernel ./"${MIPS_DIR}"vmlinux_mips.0-4-4kc-malta -nographic -drive format=qcow2,file=./"${MIPS_DIR}"debian_wheezy_mips_standard.qcow2 -net user,hostfwd=tcp::2222-:22 -net nic,model=e1000 -append "root=/dev/sda1 rw console=ttyS0" 
+	sudo qemu-system-mips -M malta -m 1024 -kernel ./"${MIPS_DIR}"vmlinux-3.2.0-4-4kc-malta -nographic -drive format=qcow2,file=./"${MIPS_DIR}"debian_wheezy_mips_standard.qcow2 -net user,hostfwd=tcp::2222-:22 -net nic,model=e1000 -append "root=/dev/sda1 rw console=ttyS0" 
 
 elif [ "${1}" = "mipsel" ]; then
-        sudo qemu-system-mipsel -M malta -m 256 -kernel ./"${MIPSEL_DIR}"vmlinux_mipsel.0-4-4kc-malta -nographic -hda ./"${MIPSEL_DIR}"debian_wheezy_mipsel_standard.qcow2 -net user,hostfwd=tcp::2222-:22,hostfwd=tcp::9000-:9000 -net nic,model=e1000 -append "root=/dev/sda1 rw console=ttyS0"
+        sudo qemu-system-mipsel -M malta -m 256 -kernel ./"${MIPSEL_DIR}"vmlinux-3.2.0-4-4kc-malta -nographic -hda ./"${MIPSEL_DIR}"debian_wheezy_mipsel_standard.qcow2 -net user,hostfwd=tcp::2222-:22,hostfwd=tcp::9000-:9000 -net nic,model=e1000 -append "root=/dev/sda1 rw console=ttyS0"
 elif [ "${1}" = "arm" ]; then
-	sudo qemu-system-arm -M vexpress-a9 -m 256 -kernel ./"${ARM_DIR}"vmlinuz_arm -initrd ./"${ARM_DIR}"initrd.img-3.2.0-4-vexpress -nographic -drive if=sd,file=./"${ARM_DIR}"debian_wheezy_armhf_standard.qcow2 -net user,hostfwd=tcp::2222-:22,hostfwd=tcp::9000-:9000 -net nic,model=e1000 -append "root=/dev/sda1 rw console=ttyS0"
+	sudo qemu-system-arm -M vexpress-a9 -m 256 -kernel ./"${ARM_DIR}"vmlinuz-3.2.0-4-vexpress -initrd ./"${ARM_DIR}"initrd.img-3.2.0-4-vexpress -nographic -drive if=sd,file=./"${ARM_DIR}"debian_wheezy_armhf_standard.qcow2 -net user,hostfwd=tcp::2222-:22,hostfwd=tcp::9000-:9000 -net nic,model=e1000 -append "root=/dev/sda1 rw console=ttyS0"
 fi
 
 
--- setup.sh
+++ setup.sh
@@ -7,3 +7,14 @@ wget -P ./linux/Mipsel https://people.debian.org/~aurel32/qemu/mipsel/vmlinux-3.
 wget -P ./linux/Arm https://people.debian.org/~aurel32/qemu/armhf/debian_wheezy_armhf_standard.qcow2
 wget -P ./linux/Arm https://people.debian.org/~aurel32/qemu/armhf/initrd.img-3.2.0-4-vexpress
 wget -P ./linux/Arm https://people.debian.org/~aurel32/qemu/armhf/vmlinuz-3.2.0-4-vexpress
+
+
+sudo apt install binwalk
+sudo apt install qemu-system-mips
+sudo apt install qemu-system-arm
+sudo apt install python 
+sudo apt install python3
+sudo apt install python-pip
+sudo pip install nose coverage
+sudo apt-get install libqt4-opengl python3-opengl python3-pyqt4 python3-pyqt4.qtopengl python3-numpy python3-scipy python3-pip
+sudo apt-get install mtd-utils gzip bzip2 tar arj lhasa p7zip p7zip-full cabextract cramfsswap squashfs-tools sleuthkit default-jdk lzop srecord
